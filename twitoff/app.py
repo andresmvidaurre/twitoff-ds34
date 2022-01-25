837bef1b7049762af7954c6ba0e5c22c14243c5e
Andres Vidaurre <92952267+andresmvidaurre@users.noreply.github.com>
2022-01-25 09:22:15 -0800

    adds three new packages and adds a .gitignore file

--- twitoff/app.py
+++ twitoff/app.py
@@ -39,16 +39,32 @@ def create_app():
         # Make two new users
         andres = User(id=1, username='andres')
         caleb = User(id=2, username='caleb')
+        satan = User(id=3, username='satan')
+        satansdog = User(id=4, username='satans dog')
+        andresinhell = User(id=5, username='andres in hell')
+        calebinhell = User(id=6, username='caleb in hell')
 
         # Make two tweets
-        tweet1 = Tweet(id=1, text='this is andres tweet', user=andres)
-        tweet2 = Tweet(id=2, text='this is calebs tweet', user=caleb)
+        tweet1 = Tweet(id=1, text='Hi I am andres', user=andres)
+        tweet2 = Tweet(id=2, text='Hi I am caleb', user=caleb)
+        tweet3 = Tweet(id=3, text='welcome, to hell', user=satan)
+        tweet4 = Tweet(id=4, text='be careful with that chalice, its poison', user=satansdog)
+        tweet5 = Tweet(id=5, text='whats in this chalice. It looks delicious.', user=andresinhell)
+        tweet6 = Tweet(id=6, text='omg, youre here too?!', user=calebinhell)
 
         # Inserting into the DB when working with SQLite directly
         DB.session.add(andres)
         DB.session.add(caleb)
+        DB.session.add(satan)
+        DB.session.add(satansdog)
+        DB.session.add(andresinhell)
+        DB.session.add(calebinhell)
         DB.session.add(tweet1)
         DB.session.add(tweet2)
+        DB.session.add(tweet3)
+        DB.session.add(tweet4)
+        DB.session.add(tweet5)
+        DB.session.add(tweet6)
 
         # Commit the DB changes
         DB.session.commit()
