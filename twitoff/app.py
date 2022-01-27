d7e2cf493cf92303554572870a4471f16671e1ac
Andres Vidaurre <92952267+andresmvidaurre@users.noreply.github.com>
2022-01-27 09:22:47 -0800

    adds logistic regregression and updates routes for final working app

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, User, Tweet
 from os import getenv
 from .twitter import add_or_update_user
+from .predict import predict_user
 
 
 
@@ -22,21 +23,19 @@ def create_app():
     @app.route("/")
     def home_page():
         # query for all users in the database
-        users = User.query.all()
-        print(users)
-        return render_template('base.html', title='Home', users=users)
+        return render_template('base.html', title='Home', users=User.query.all())
 
-    @app.route('/populate')
+   # @app.route('/populate')
     # Test my database functionality
     # by inserting some fake data into the DB
-    def populate():
+    #def populate():
 
-        add_or_update_user('nasa')
-        add_or_update_user('euphoriaHBO')
-        add_or_update_user('ryanallred')
+      #  add_or_update_user('nasa')
+     #   add_or_update_user('euphoriaHBO')
+       # add_or_update_user('ryanallred')
 
         
-        return render_template('base.html', title='Populate')
+        #return render_template('base.html', title='Populate')
 
     @app.route('/update')
     # Test my database functionality
@@ -48,7 +47,7 @@ def create_app():
             add_or_update_user(username)
 
         
-        return render_template('base.html', title='Update User Tweets')
+        return render_template('base.html', title='All Users have been updated')
 
 
     @app.route('/reset')
@@ -57,7 +56,35 @@ def create_app():
         DB.drop_all()
         DB.create_all()
         return render_template('base.html', title='Reset Database')
+    
+    # user route is a more traditional API endpoint
+    # This endpoint can only accept certain kinds of http requests.
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<username>', methods=['GET'])
+    def user(username=None, message=''):
+        username = username or request.values['user_name']
+        try:
+            if request.method=='POST':
+                add_or_update_user(username)
+                message = f'User {username} has been successfully added!'
+            tweets = User.query.filter(User.username == username).one().tweets
+        except Exception as error:
+            message = f'Error adding {username}: {error}'
+            tweets = []
+        return render_template('user.html', titel=username, tweets=tweets, message=message)
 
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user0, user1 = sorted([request.values['user0'], request.values['user1']])
+
+        if user0 == user1:
+            message = 'Cannot compare user to themselves'
+        else:
+            prediction = predict_user(user0, user1, request.values['tweet_text'])
+            message = "'{}' is more likely to be said by {} than {}".format(request.values['tweet_text'],
+                                                                            user1 if prediction else user0,
+                                                                            user0 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
 
     return app
 
